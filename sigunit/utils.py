2ac390408163dcaa2988f6fffe0985c905134e57
G181124 <92919318+G181124@users.noreply.github.com>
2025-04-07 20:19:21 +0800

    Delete sigunit directory

--- sigunit/utils.py
+++
@@ -1,9 +0,0 @@
-
-def color_text(text, color="green"):
-    colors = {
-        "green": "\033[92m",
-        "red": "\033[91m",
-        "yellow": "\033[93m",
-        "reset": "\033[0m"
-    }
-    return f"{colors.get(color, '')}{text}{colors['reset']}"
