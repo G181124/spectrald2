bdb2b42f42b4e49b3bbd41df5a878bf0c71415c6
G181124 <92919318+G181124@users.noreply.github.com>
2025-04-07 23:27:15 +0800

    Update utils.py

--- sigunit/utils.py
+++ sigunit/utils.py
@@ -1,9 +1,13 @@
-
 def color_text(text, color="green"):
     colors = {
         "green": "\033[92m",
         "red": "\033[91m",
         "yellow": "\033[93m",
+        "blue": "\033[94m",
+        "magenta": "\033[95m",
+        "cyan": "\033[96m",
+        "white": "\033[97m",
+        "bold": "\033[1m",
         "reset": "\033[0m"
     }
     return f"{colors.get(color, '')}{text}{colors['reset']}"
